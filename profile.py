82f4e31cb39573836f4acb20734364f903942c0d
Umakant Kulkarni <UmakantKulkarni@users.noreply.github.com>
2025-06-14 15:19:11 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """Parameterized profile to create a multisite experiment via multiple LANs.
 
-Each “LAN” block lets you choose:
+Each "LAN" block lets you choose:
  - which CloudLab site (aggregate) to use,
  - how many nodes to request on that LAN,
  - which OS image each node should run,
  - which hardware type each node should use.
 
-Click “Add another LAN block” to create LAN1, LAN2, etc., and at the end
+Click "Add another LAN block" to create LAN1, LAN2, etc., and at the end
 all of those LANs will be interconnected.
 """
 
@@ -24,19 +24,19 @@ request = pc.makeRequestRSpec()
 # -------------------------------------------------------------------
 imageList = [
     ('default', 'Default Image'),
-    ('urn:publicid:IDN+cloudlab.umass.edu+image+sfcs-PG0:ztx_ubuntu22',        'UBUNTU 22.04 ZTX'),
-    ('urn:publicid:IDN+cloudlab.umass.edu+image+sfcs-PG0:p4_sdn',             'P4-SDN UBUNTU22'),
-    ('urn:publicid:IDN+cloudlab.umass.edu+image+sfcs-PG0:BG_QOE_PRED_P4_SDN','QOE-PRED-P4-SDN'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD',        'UBUNTU22-64-STD'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU24-64-BETA',       'UBUNTU24-64-BETA'),
-    ('urn:publicid:IDN+cloudlab.umass.edu+image+sfcs-PG0:ubuntu24lts',       'UBUNTU 24.04 LTS'),
+    ('urn:publicid:IDN+cloudlab.umass.edu+image+sfcs-PG0:ztx_ubuntu22', 'UBUNTU 22.04 ZTX'),
+    ('urn:publicid:IDN+cloudlab.umass.edu+image+sfcs-PG0:p4_sdn',        'P4-SDN UBUNTU22'),
+    ('urn:publicid:IDN+cloudlab.umass.edu+image+sfcs-PG0:BG_QOE_PRED_P4_SDN', 'QOE-PRED-P4-SDN'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD',      'UBUNTU22-64-STD'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU24-64-BETA',     'UBUNTU24-64-BETA'),
+    ('urn:publicid:IDN+cloudlab.umass.edu+image+sfcs-PG0:ubuntu24lts',     'UBUNTU 24.04 LTS'),
     ('urn:publicid:IDN+utah.cloudlab.us+image+sfcs-PG0:sfc_u20_k8s_5g_uth','UBUNTU 20.04 K8s 5G Utah'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD',        'UBUNTU 18.04'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-ARM',        'UBUNTU22-64-ARM'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD',        'UBUNTU 20.04'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU16-64-STD',        'UBUNTU 16.04'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//CENTOS7-64-STD',         'CENTOS 7'),
-    ('urn:publicid:IDN+emulab.net+image+emulab-ops//FBSD112-64-STD',         'FreeBSD 11.2'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD',      'UBUNTU 18.04'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-ARM',      'UBUNTU22-64-ARM'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD',      'UBUNTU 20.04'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU16-64-STD',      'UBUNTU 16.04'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//CENTOS7-64-STD',       'CENTOS 7'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//FBSD112-64-STD',       'FreeBSD 11.2'),
 ]
 
 # -------------------------------------------------------------------
@@ -44,10 +44,10 @@ imageList = [
 # -------------------------------------------------------------------
 ps = pc.defineStructParameter(
     'lans', 'LANs', [],
-    multiValue       = True,
-    min              = 1,
-    hide             = False,
-    multiValueTitle  = 'Add another LAN block',
+    multiValue      = True,
+    min             = 1,
+    hide            = False,
+    multiValueTitle = 'Add another LAN block',
     members = [
         portal.Parameter(
             'site', 'CloudLab site (aggregate)',
@@ -55,7 +55,8 @@ ps = pc.defineStructParameter(
             longDescription='Select which CloudLab site to run this LAN on.'
         ),
         portal.Parameter(
-            'count', 'Number of Nodes', portal.ParameterType.INTEGER, 1,
+            'count', 'Number of Nodes',
+            portal.ParameterType.INTEGER, 1,
             longDescription='How many nodes to request on this LAN.'
         ),
         portal.Parameter(
@@ -82,7 +83,7 @@ for idx, lanblock in enumerate(params.lans):
             'LAN %d: node count must be at least 1.' % (idx + 1),
             ['lans[%d].count' % idx]
         ))
-    # we allow an empty site (use default) if desired
+    # empty site uses default
 
 pc.verifyParameters()
 
@@ -92,7 +93,7 @@ pc.verifyParameters()
 all_nodes = []
 
 for lanblock in params.lans:
-    # derive short name from site URN, e.g. urn:...+emulab.net+... → "emulab"
+    # derive short name from site URN, e.g. urn:...+emulab.net+... -> "emulab"
     if lanblock.site:
         parts = lanblock.site.split('+')
         short = parts[1].split('.')[0] if len(parts) > 1 else 'site'
